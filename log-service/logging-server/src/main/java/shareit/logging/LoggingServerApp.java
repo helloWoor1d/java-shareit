fd18d0440b6942ec14c05b593b976b533b6ee2fb
mira <pavuuuuuk@yandex.ru>
2025-08-25 23:17:42 +0300

    feat: использование eureka-server для взаимного обнаружения auth-service и main-service

--- log-service/logging-server/src/main/java/shareit/logging/LoggingServerApp.java
+++ log-service/logging-server/src/main/java/shareit/logging/LoggingServerApp.java
@@ -2,7 +2,9 @@ package shareit.logging;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class LoggingServerApp {
     public static void main(String[] args) {
